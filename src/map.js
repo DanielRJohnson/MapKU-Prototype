7883fe230cc21cf2073b44be5b625074d77dff19
Maokun Huang <67557427+Harry-Huang38@users.noreply.github.com>
2020-10-20 17:10:15 -0500

    Add info Panels and change Marker image

--- src/map.js
+++ src/map.js
@@ -1,6 +1,49 @@
 function initMap() {
-    const snowHall = {lat: 38.9586897463383, lng: -95.24913753763796};
-    const directionsService = new google.maps.DirectionsService();
+	var markerImage = new google.maps.MarkerImage('image/Jayhawk.png',
+                new google.maps.Size(100, 100),
+                new google.maps.Point(0, 0),
+                new google.maps.Point(15, 15));
+	const beaches = [
+        //AnschutzLibrary
+        	['<div id="content">'+'<div id="siteNotice">'+"</div>" +
+    '<h1 id="firstHeading" class="firstHeading"><u>Anschutz Library</u></h1>' +
+    '<div id="bodyContent">' +
+            "<h4>Address: 1301 Hoch Auditoria Dr, Lawrence, KS 66045, United States</h4>, <div align='center'> <img src='image/AnschutzLibrary.jpg'></div>" +
+    "</div>", 38.957325, -95.249661, "Anschutz Library"],
+        //KU Engineering
+		[	    '<div id="content">'+'<div id="siteNotice">'+"</div>" +
+    '<h1 id="firstHeading" class="firstHeading"><u>KU engineering</u></h1>' +
+    '<div id="bodyContent">' +
+			"<h4>Address: Eaton Hall, 1520 W 15th St, Lawrence, KS 66045, United States</h4>, <div align='center'><img src='image/KUEngineering.jpg'></div> , </p>" +
+    "</div>", 38.95781445088452, -95.25263124321691, "KU Engineering"],
+        //Fraser Hall
+		[	    '<div id="content">'+'<div id="siteNotice">'+"</div>" +
+    '<h1 id="firstHeading" class="firstHeading"><u>Fraser Hall</u></h1>' +
+    '<div id="bodyContent">' +
+			"<h4>Address: 1415 Jayhawk Blvd, Lawrence, KS 66045, United States</h4>, <div align='center'><img src='image/FraserHall.jpg'></div> , </p>" +
+    "</div>", 38.957273866157685, -95.24356786041308,"Fraser Hall"],
+        //Wescoe Hall
+		[    	    '<div id="content">'+'<div id="siteNotice">'+"</div>" +
+    '<h1 id="firstHeading" class="firstHeading"><u>Wescoe Hall</u></h1>' +
+    '<div id="bodyContent">' +
+			"<h4>Address: 1445 Jayhawk Boulevard, Lawrence, KS 66045</h4>, <div align='center'><img src='image/WescoeHall.jpg'></div> , </p>" +
+                "</div>", 38.957784622454255, -95.24777752972496,"Wescoe Hall"],
+        //Snow hall
+		[				    	    '<div id="content">'+'<div id="siteNotice">'+"</div>" +
+    '<h1 id="firstHeading" class="firstHeading"><u>Snow hall</u></h1>' +
+    '<div id="bodyContent">' +
+			"<h4>Address: 1460 Jayhawk Blvd, Lawrence, KS 66045, United States</h4>, <div align='center'><img src='image/SnowHall.png'></div> , </p>" +
+                                "</div>", 38.95892226130865, -95.24899211623296, "Snow Hall"],
+        //Lied Center
+		[								    	    '<div id="content">'+'<div id="siteNotice">'+"</div>" +
+    '<h1 id="firstHeading" class="firstHeading"><u>Lied Center</u></h1>' +
+    '<div id="bodyContent">' +
+			"<h4>Address: 1600 Stewart Dr, Lawrence, KS 66045, United States</h4>, <div align='center'><img src='image/LiedCenter.jpg'></div> , </p>" +
+    "</div>", 38.95511168388419, -95.26276438739704, "Lied Center"]
+	];
+	
+
+	const directionsService = new google.maps.DirectionsService();
     const directionsRenderer = new google.maps.DirectionsRenderer();
     const map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 38.957235, lng: -95.248962 },
@@ -28,11 +71,15 @@ function initMap() {
         directionsRenderer.setMap(null);
         document.getElementById("searchBox").value = "";*/
     })
-    let addMarker = (latLng, title) => {
-        let marker = new google.maps.Marker({
-            position: {lat: latLng.lat, lng: latLng.lng},
+    
+	let addMarker = (lati, lngi, title, name) => {
+
+		let marker = new google.maps.Marker({
+			animation: google.maps.Animation.DROP,
+            position: {lat: lati, lng: lngi},
             map,
-            title: title
+			title: name,
+            icon: markerImage
         });
         marker.setOpacity(.4);
         marker.addListener('mouseover', function() {
@@ -43,9 +90,9 @@ function initMap() {
                 marker.setOpacity(0.4);
             }
         });
-    
-        const infowindow = new google.maps.InfoWindow({
-            content: title
+        
+		const infowindow = new google.maps.InfoWindow({
+			content: title
         });
         
         marker.addListener("click", () => {
@@ -53,18 +100,23 @@ function initMap() {
         });
 
         marker.addListener("dblclick", () => {
-            myRoute.addToRoute(title);
+            myRoute.addToRoute(name);
             infowindow.close();
         })
     }
-
-    addMarker(snowHall, "Snow Hall");
+ 
+	for (let i = 0; i < beaches.length; i++) {
+		const beach = beaches[i];
+			
+		addMarker(beach[1], beach[2], beach[0], beach[3]);
+	}
 
     map.addListener("click" , (mouseEvent) => {
         console.log(mouseEvent.latLng.lat(), mouseEvent.latLng.lng());
     });
 }
 
+                
 function mapRoute(directionsService, directionsRenderer, route){
     directionsService.route(
         {
